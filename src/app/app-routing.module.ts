a8e3fcd080c6bdd4485fe1f9a7e3f46a1b596d57
viviane Oliveira <s.vivianedeoliveira@gmail.com>
2021-10-27 20:24:48 -0300

    commit inicial, subindo arquivos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [ //local onde passamos nossa estrutura de rotas
+  { path: 'login', component: LoginComponent },
+  // Path é o caminho URL (login) declarado no HTML e o componente a ser carregado (loginComponent)
+  { path: '', pathMatch: 'full', redirectTo: 'login' }
+  //Path Default caminho URL (login)
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
